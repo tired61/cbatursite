a7930369eaef4cea8f3f9d7360331cb4c1ec9d53
bora <67245246+tired61@users.noreply.github.com>
2025-09-19 05:21:46 +0300

    feat: responsive features grid (3 kart)

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById("sayHiBtn").addEventListener("click", () => {
   alert("Selam Bora! 🎯 İlk etkileşimi yaptık.");
 });
+// selamlama
+document.getElementById("sayHiBtn").addEventListener("click", () => {
+  alert("Selam Bora! 🎯 Navbar + hero hazır.");
+});
+
+// mobil menü
+const menuBtn = document.getElementById("menuToggle");
+const links = document.getElementById("navLinks");
+menuBtn.addEventListener("click", () => {
+  const isOpen = links.style.display === "block";
+  links.style.display = isOpen ? "none" : "block";
+});
